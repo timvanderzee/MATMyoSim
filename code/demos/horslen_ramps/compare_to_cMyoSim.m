0009cacae5fbd86135a9abb42e3b166ec88626a6
Tim <timvdzee@hotmail.com>
2024-06-25 10:25:47 +0200

    added submaximal pCas using batch

--- code/demos/horslen_ramps/compare_to_cMyoSim.m
+++ code/demos/horslen_ramps/compare_to_cMyoSim.m
@@ -1,37 +1,62 @@
+close all
+
+% Variables
+pCa_values = [4.5, 6.1, 6.2, 6.4, 9.0];
+prots = 5:-1:1;
+
+for j = 1:length(pCa_values)
+
 % load MATMyoSim output
-load([MATMyoSim_path,'\code\temp\horslen_ramp_output.myo'],'-mat')
+load([MATMyoSim_path,'\code\demos\horslen_ramps\temp\sim_output\',num2str(pCa_values(j)*10),'\results_',num2str(pCa_values(j)*10),'.myo'],'-mat')
 
 % load cMyoSim output
-cMyoSim_output = readmatrix([cMyoSim_path,'\progress\1\sim_prot_5_1_r1.txt']);
+cMyoSim_output = readmatrix([cMyoSim_path,'\progress\1\sim_prot_',num2str(prots(j)),'_1_r1.txt']);
 
-clf
 figure(1);
 color = get(gca,'colororder');
 
-subplot(121);
-plot(sim_output.time_s, sim_output.muscle_force); hold on
-plot(cMyoSim_output(:,1), cMyoSim_output(:,3),'--','color',color(1,:));
+subplot(2,4,1:4);
+plot(sim_output.time_s, sim_output.muscle_force,'color',color(j,:)); hold on
+plot(cMyoSim_output(:,1), cMyoSim_output(:,3),'--','color',color(j,:));
 
 ylabel('Force (N m^{-2})');
 legend('MATMyoSim','cMyoSim','location','best')
-title('Forces')
-
-subplot(122)
-plot(sim_output.time_s,[sim_output.M1 sim_output.M2 sim_output.M3 sim_output.M4]); hold on
-
-for i = 1:4
-    d = 4-i;
-    plot(cMyoSim_output(:,1), cMyoSim_output(:,end-d),'--','color',color(i,:))
-end
-
+title('Force')
+box off
 xlim([0 1.2])
-legend('SRX','DRX','Pre stroke','Post stroke','location','best')
-title('States')
+xlabel('Time (s)')
 
-for i = 1:2
-    subplot(1,2,i)
-    xlim([0 1.2])
-    box off
-    xlabel('Time (s)')
+subplot(245)
+plot(sim_output.time_s,sim_output.M1,'color',color(j,:)); hold on
+plot(cMyoSim_output(:,1), cMyoSim_output(:,end-3),'--','color',color(j,:))
+title('SRX')
+box off
+xlim([0 1.2])
+xlabel('Time (s)')
+
+subplot(246)
+plot(sim_output.time_s,sim_output.M2,'color',color(j,:)); hold on
+plot(cMyoSim_output(:,1), cMyoSim_output(:,end-2),'--','color',color(j,:))
+title('DRX')
+box off
+xlim([0 1.2])
+xlabel('Time (s)')
+
+subplot(247)
+plot(sim_output.time_s,sim_output.M3,'color',color(j,:)); hold on
+plot(cMyoSim_output(:,1), cMyoSim_output(:,end-1),'--','color',color(j,:))
+title('Pre-stroke')
+box off
+xlim([0 1.2])
+xlabel('Time (s)')
+
+subplot(248)
+plot(sim_output.time_s,sim_output.M4,'color',color(j,:)); hold on
+plot(cMyoSim_output(:,1), cMyoSim_output(:,end-0),'--','color',color(j,:))
+title('Post-stroke')
+box off 
+xlim([0 1.2])
+xlabel('Time (s)')
+
+
 end
-
--- code/demos/horslen_ramps/demo_horslen_ramp.m
+++ code/demos/horslen_ramps/demo_horslen_ramp.m
@@ -1,34 +1,14 @@
 function demo_horslen_ramp
 
-
-% Variables
-protocol_file_string = 'horslen_ramp_protocol.txt';
-model_parameters_json_file_string = 'horslen_ramp_parameters.json';
-options_file_string = 'horslen_ramp_options.json';
-model_output_file_string = '../../temp/horslen_ramp_output.myo';
-
 % Make sure the path allows us to find the right files
 addpath(genpath('../../../code'));
 
-% Run a simulation
-sim_output = simulation_driver( ...
-    'simulation_protocol_file_string', protocol_file_string, ...
-    'model_json_file_string', model_parameters_json_file_string, ...
-    'options_json_file_string', options_file_string, ...
-    'output_file_string', model_output_file_string);
+% Variables
+batch_file_string = 'horslen_ramp_batch.json';
 
-% Load it back up and display to show how that can be done
-sim = load(model_output_file_string,'-mat')
-sim_output = sim.sim_output
+% Load the batch data
+batch_json = loadjson(batch_file_string);
+batch_data = batch_json.MyoSim_batch;
 
-figure(2);
-clf;
-subplot(2,1,1);
-plot(sim_output.time_s,sim_output.muscle_force,'b-'); hold on
-% plot(sim_output.time_s,sim_output.c_force,'b--');
-% plot(sim_output.time_s,sim_output.muscle_force-sim_output.c_force,'b:');
-ylabel('Force (N m^{-2})');
-subplot(2,1,2);
-plot(sim_output.time_s,sim_output.hs_length,'b-');
-ylabel('Half-sarcomere length (nm)');
-xlabel('Time (s)');
+% Run the batch
+run_batch(batch_data);
